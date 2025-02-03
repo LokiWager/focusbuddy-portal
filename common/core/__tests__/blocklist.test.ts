e88b044a3ac9879e07f8708cfc7640c2e6a99b3b
Loki's Wager <32408858+LokiWager@users.noreply.github.com>
2025-02-03 08:06:05 +0800

    support local storage for cache (#3)
    * [feature] add local storage
    
    * add pr template

---
+++ common/core/__tests__/blocklist.test.ts
@@ -0,0 +1,22 @@
+import { test, expect } from "vitest";
+import { parseDomainFromURL } from "../blocklist";
+
+test("parseDomainFromURL", () => {
+  expect(parseDomainFromURL("https://www.google.com")).toBe("www.google.com");
+  expect(parseDomainFromURL("https://www.google.com/")).toBe("www.google.com");
+  expect(parseDomainFromURL("https://www.google.com/search?q=hello")).toBe(
+    "www.google.com"
+  );
+  expect(parseDomainFromURL("https://www.google.com/search?q=hello#")).toBe(
+    "www.google.com"
+  );
+  expect(parseDomainFromURL("https://www.google.com/search?q=hello#foo")).toBe(
+    "www.google.com"
+  );
+  expect(
+    parseDomainFromURL("https://www.google.com/search?q=hello#foo/bar")
+  ).toBe("www.google.com");
+  expect(
+    parseDomainFromURL("https://www.google.com/search?q=hello#foo/bar/")
+  ).toBe("www.google.com");
+});
