32aab9eb8053cc7735786723f92b50ecc784ee5d
Clara <69889014+ChunhaoZhu@users.noreply.github.com>
2025-02-03 21:41:48 -0500

    11 focustimer pop up initialization (#4)
    * popup mainpage
    
    * count-down pages
    
    * error msg
    
    * code cleanup
    
    * minor fix
    
    * remove pause state

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,15 +1,50 @@
+import { useState } from "react";
 import { browser } from "wxt/browser";
+import FocusTimer from "./routes/focusTimer/focusTimer";
+import BlockList from "./routes/blocklist/blocklist";
+import "./style.css"; // Import the CSS file
 
 const DASHBOARD_URL = browser.runtime.getURL("/dashboard.html");
+const SETTINGS_URL = browser.runtime.getURL("/dashboard.html#/settings");
 
 function App() {
+  const [activeTab, setActiveTab] = useState<"tab1" | "tab2">("tab1");
+
   return (
-    <div>
-      <p>
-        <a href={DASHBOARD_URL} target="_blank" rel="noreferrer">
-          Open dashboard
+    <div className="app-container">
+      {/* Links */}
+      <p className="app-links">
+        <a href={DASHBOARD_URL} target="_blank" rel="noreferrer" className="focus-buddy-link">
+          <img src="/icon/logo.png" alt="Focus Buddy" />
+          Focus Buddy
+        </a>
+        <a href={SETTINGS_URL} target="_blank" rel="noreferrer">
+          <img src="/icon/settings-icon.png" alt="Focus Buddy" className="settings-icon" />
         </a>
       </p>
+
+      {/* Tab buttons */}
+      <div className="tabs-container">
+        <button
+          onClick={() => setActiveTab("tab1")}
+          className={`tab-button ${activeTab === "tab1" ? "active-tab" : ""}`}
+        >
+          <img src="/icon/focus-tab-icon.png" alt="Focus Tab" />
+          Focus Timer
+        </button>
+        <button
+          onClick={() => setActiveTab("tab2")}
+          className={`tab-button ${activeTab === "tab2" ? "active-tab" : ""}`}
+        >
+          <img src="/icon/block-tab-icon.png" alt="Block Tab" />
+          Block Sites
+        </button>
+      </div>
+
+      {/* Component display */}
+      <div className="content-container">
+        {activeTab === "tab1" ? <FocusTimer /> : <BlockList />}
+      </div>
     </div>
   );
 }
---
+++ entrypoints/popup/routes/blocklist/blockList.tsx
@@ -0,0 +1,12 @@
+
+
+const BlockList = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-bold">BlockLists</h2>
+      <p>BlockList content goes here...</p>
+    </div>
+  );
+};
+
+export default BlockList;
