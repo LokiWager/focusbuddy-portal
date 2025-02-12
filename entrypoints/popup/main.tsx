b94038b5c6b5bd701ac7de2fb622e8e6653885e5
Loki's Wager <32408858+LokiWager@users.noreply.github.com>
2025-02-12 08:37:37 +0800

    remove unused import (#16)

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import {
   AuthContext,
   AuthProvider,
 } from "@/common/components/auth/AuthContext.tsx";
-import { Login } from "@/common/components/auth/login.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 import { Layout } from "./Layout.tsx";
 
 const queryClient = new QueryClient();
