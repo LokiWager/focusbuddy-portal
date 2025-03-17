f08170e95dd15e119141049b011df3f4928ffed5
Yingru Pan <panyingru0926@gmail.com>
2025-03-17 19:04:17 -0400

    update modify function for focus sessions

--- common/api/api.ts
+++ common/api/api.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 import { useAuthFetch } from "../components/auth/AuthContext";
 import {
   addToLocalStorage,
