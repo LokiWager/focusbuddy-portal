fbaca76c3efcb66be5a570ae7c910b1f0eafcefa
ChunhaoZhu <15962613747@163.com>
2025-03-28 21:28:30 -0400

    change

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { WxtVitest } from "wxt/testing";
 
 export default defineConfig({
   plugins: [
-    // @ts-expect-error - WxtVitest is not part of the official Vite plugin list
     WxtVitest(),
   ],
 });
